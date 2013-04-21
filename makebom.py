3e48b729f8ffcfc8205c2975a3b6a7d0d9d6d9ad
Nathan Bergey <nathan.bergey@gmail.com>
2013-04-21 03:12:39 -0700

    genorated csv and textile page from raw Digikey order quote

--- makebom.py
+++ makebom.py
@@ -7,17 +7,28 @@ with open(sys.argv[1], 'r') as digikey:
     for line in digikey:
         li = line.split(',')
 
-        digikey_part =      li[0].strip()
-        manufacturer =      li[1].strip()
-        manuf_part   =      li[2].strip()
-        part         =      li[3].strip()
-        num          =  int(li[4].strip())
-        desc         =      li[7].strip()
+        digikey_part =       li[0].strip()
+        manufacturer =       li[1].strip()
+        manuf_part   =       li[2].strip()
+        desc         =       li[3].strip()
+        part         =       li[4].strip()
+        num          =   int(li[5].strip())
+        price        = float(li[6].strip('"').strip())
 
         #print num, part, manuf_part, desc.title(), dikikey_part
         digilink = "http://www.digikey.com/product-detail/en/%s/%s/" % (manuf_part, digikey_part)
-        bom.append({'n': num, 'part': part, 'desc':  manuf_part+" "+desc.title(), 'digikey': digikey_part, 'digilink': digilink, 'price': 1.00})
-    
+        bom.append({'n': num, 'part': part, 'desc':  manuf_part+" "+desc.title(), 'digikey': digikey_part, 'digilink': digilink, 'price': price})
+
+with open("ISS-Notify-BOM.csv", 'w') as csv:
+    csv.write("#num, name, desc, digikey part, price, line amount\n")
+    for item in bom:
+        num = item['n']
+        price = item['price']
+        total = price * num
+        cells = (num, item['part'], item['desc'], item['digikey'], price, total)
+        csv.write('%d,%s,%s,%s,%0.2f,%0.2f\n' % cells)
+
+
 with open("bom.textile", 'w') as f:
     f.write("""---
 layout: harddoc
@@ -30,14 +41,17 @@ h1. Bill Of Materials
 table(table table-striped).
 |_. # |_. Part No. |_. Item |_. Order No. |_. Price |_. Total |
 """)
+    grand_total = 0
     for item in bom:
         num = item['n']
         price = item['price']
         total = price * num
+        grand_total += total
         cells = (num, item['part'], item['desc'], item['digikey'], item['digilink'], price, total)
 
         f.write('| %d | %s | %s | "%s":%s |>. $%0.2f |>. $%0.2f |\n' % cells)
 
+    f.write('|_. Total: | | | | |>. $%0.2f |\n' % (grand_total))
     f.write("""
 
 h3. Download BOM
